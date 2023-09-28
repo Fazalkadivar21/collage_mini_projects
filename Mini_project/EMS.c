0df3df39667f161c9bf9f4acd77f09ad91f87585
Fazal Kadivar <118613580+Fazalkadivar21@users.noreply.github.com>
2023-09-28 17:13:06 +0530

    Update EMS.c

--- Mini_project/EMS.c
+++ Mini_project/EMS.c
@@ -17,7 +17,7 @@ typedef struct {
 } Employee;
 
 // Function to add an employee
-void addEmployee(Employee** employees, int* numEmployees) {
+void addEmployee(Employee*** employees, int* numEmployees) {
     Employee newEmployee;
     printf("Enter Employee ID: ");
     scanf("%d", &newEmployee.empID);
@@ -31,29 +31,37 @@ void addEmployee(Employee** employees, int* numEmployees) {
     scanf(" %50s", newEmployee.contact.email);
 
     // Allocate memory for the new employee
-    *employees = realloc(*employees, (*numEmployees + 1) * sizeof(Employee));
-    if (*employees == NULL) {
+    Employee** temp = (Employee**)realloc(*employees, (*numEmployees + 1) * sizeof(Employee*));
+    if (temp == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    *employees = temp;
+
+    // Allocate memory for the new employee record
+    (*employees)[*numEmployees] = (Employee*)malloc(sizeof(Employee));
+    if ((*employees)[*numEmployees] == NULL) {
         printf("Memory allocation failed.\n");
         exit(1);
     }
 
-    // Add the new employee to the array
-    (*employees)[*numEmployees] = newEmployee;
+    // Copy the new employee data into the allocated record
+    *(*employees)[*numEmployees] = newEmployee;
     (*numEmployees)++;
 
     printf("Employee added successfully!\n");
 }
 
 // Function to search for an employee by ID
-void searchEmployee(const Employee* employees, int numEmployees, int targetID) {
+void searchEmployee(const Employee** employees, int numEmployees, int targetID) {
     for (int i = 0; i < numEmployees; i++) {
-        if (employees[i].empID == targetID) {
+        if (employees[i]->empID == targetID) {
             printf("Employee found:\n");
-            printf("Employee ID: %d\n", employees[i].empID);
-            printf("Name: %s\n", employees[i].name);
-            printf("Salary: %.2f\n", employees[i].salary);
-            printf("Phone: %s\n", employees[i].contact.phone);
-            printf("Email: %s\n", employees[i].contact.email);
+            printf("Employee ID: %d\n", employees[i]->empID);
+            printf("Name: %s\n", employees[i]->name);
+            printf("Salary: %.2f\n", employees[i]->salary);
+            printf("Phone: %s\n", employees[i]->contact.phone);
+            printf("Email: %s\n", employees[i]->contact.email);
             return;
         }
     }
@@ -61,29 +69,31 @@ void searchEmployee(const Employee* employees, int numEmployees, int targetID) {
 }
 
 // Function to display all employees
-void displayEmployees(const Employee* employees, int numEmployees) {
-
+void displayEmployees(const Employee** employees, int numEmployees) {
     if (numEmployees == 0) {
         printf("No data found.\n");
         return;
     }
-    
+
     printf("\nAll Employees:\n");
     for (int i = 0; i < numEmployees; i++) {
-        printf("Employee ID: %d\n", employees[i].empID);
-        printf("Name: %s\n", employees[i].name);
-        printf("Salary: %.2f\n", employees[i].salary);
-        printf("Phone: %s\n", employees[i].contact.phone);
-        printf("Email: %s\n", employees[i].contact.email);
+        printf("Employee ID: %d\n", employees[i]->empID);
+        printf("Name: %s\n", employees[i]->name);
+        printf("Salary: %.2f\n", employees[i]->salary);
+        printf("Phone: %s\n", employees[i]->contact.phone);
+        printf("Email: %s\n", employees[i]->contact.email);
         printf("--------------------------\n");
     }
 }
 
 // Function to delete an employee by ID
-void deleteEmployee(Employee** employees, int* numEmployees, int targetID) {
+void deleteEmployee(Employee*** employees, int* numEmployees, int targetID) {
     int found = 0;
     for (int i = 0; i < *numEmployees; i++) {
-        if ((*employees)[i].empID == targetID) {
+        if ((*employees)[i]->empID == targetID) {
+            // Free the memory for the employee record
+            free((*employees)[i]);
+            
             // Shift remaining employees to cover the deleted position
             for (int j = i; j < *numEmployees - 1; j++) {
                 (*employees)[j] = (*employees)[j + 1];
@@ -99,22 +109,26 @@ void deleteEmployee(Employee** employees, int* numEmployees, int targetID) {
     }
 
     // Reallocate memory to adjust the array size
-    *employees = realloc(*employees, (*numEmployees) * sizeof(Employee));
-    if (*employees == NULL && *numEmployees > 0) {
+    Employee** temp = (Employee**)realloc(*employees, (*numEmployees) * sizeof(Employee*));
+    if (temp == NULL && *numEmployees > 0) {
         printf("Memory reallocation failed.\n");
         exit(1);
     }
+    *employees = temp;
 }
 
 // Function to free memory allocated for employees
-void freeEmployees(Employee** employees, int* numEmployees) {
+void freeEmployees(Employee*** employees, int* numEmployees) {
+    for (int i = 0; i < *numEmployees; i++) {
+        free((*employees)[i]);
+    }
     free(*employees);
     *employees = NULL;
     *numEmployees = 0;
 }
 
 int main() {
-    Employee* employees = NULL;
+    Employee** employees = NULL;
     int numEmployees = 0;
     int choice, empID;
 
@@ -135,10 +149,10 @@ int main() {
             case 2:
                 printf("Enter Employee ID to search: ");
                 scanf("%d", &empID);
-                searchEmployee(employees, numEmployees, empID);
+                searchEmployee((const Employee**)employees, numEmployees, empID);
                 break;
             case 3:
-                displayEmployees(employees, numEmployees);
+                displayEmployees((const Employee**)employees, numEmployees);
                 break;
             case 4:
                 printf("Enter Employee ID to delete: ");
